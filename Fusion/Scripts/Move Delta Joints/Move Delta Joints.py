f7d204cea6ad26783816b95d8a3fc9ff252b9825
Marc Grossman <marcgrossman@Marcs-MacBook-Pro.local>
2018-06-27 21:15:50 -0700

    seemed to be working. maybe a bug here or there, but it moved the delta and was able to save to a .json. Committing now because I've had issues recently where shit stops working for no reason.

--- Fusion/Scripts/Move Delta Joints/Move Delta Joints.py
+++ Fusion/Scripts/Move Delta Joints/Move Delta Joints.py
@@ -1,7 +1,9 @@
 import adsk.core, adsk.fusion, traceback, math, random
 import csv
 import os
+import json
 import time
+import operator
 
 pi = math.pi
 
@@ -10,6 +12,7 @@ xyzs = []
 
 rootPath = os.path.expanduser('~')
 fullPath = os.path.join(rootPath, 'Documents', 'output3.csv')
+jsonFullPath = 'Documents/data.json'
 
 app = adsk.core.Application.get()
 ui = app.userInterface
@@ -98,6 +101,12 @@ def readCSV(fullPath):
 
 
 
+def write2json(filePath, data):
+	with open(filePath, 'w') as fp:
+		# json.dump(data, fp)
+		json.dump(data, fp, sort_keys=True, indent=4, separators=(',', ': '))
+
+
 # def setRigidJoints(rigid_joints, thetas):
 # 	# user_joint0_angle.value = thetas[0]*pi/180
 # 	# user_joint1_angle.value = thetas[1]*pi/180
@@ -111,12 +120,13 @@ def setRigidJoints(rigid_joints, revolute_joints, thetas):
 	current_thetas = [j.user_angle for j in rigid_joints]
 	for Ts in zip(current_thetas, range(len(thetas))):
 		if Ts[0] != thetas[Ts[1]]:
+			idx = Ts[1]
 			#current rigid angle not equal to desired angle... free the joint then move as revolute
-			rigid_joints[Ts[1]].isSuppressed = True
-			revolute_joints[Ts[1]].jointMotion.rotationValue = thetas[0]
+			rigid_joints[idx].isSuppressed = True
+			revolute_joints[idx].jointMotion.rotationValue = thetas[idx]
 			#now set the user angle that defines the rigid joint to match
-			rigid_joints[Ts[1]].user_angle = thetas[0]
-			rigid_joints[Ts[1]].isSuppressed = False
+			rigid_joints[idx].user_angle = thetas[idx]
+			rigid_joints[idx].isSuppressed = False
 			adsk.doEvents()
 
 
@@ -139,14 +149,14 @@ def sweepJoint(joint_id, rigid_joints, revolute_joints, thetas, mobilePlatform):
 
 	
 
-def exploreLocation(rigid_joints, rev_joints):
-	#supporess one rigid joint then explore dtheta/ddist in both directions around that point
-	#the repeat for the other two joints
-	dthetas
-	dtheta = 0.0001#rad
-	for rj in zip(rigid_joints, list(range(3))):
-		rj[0].isSuppressed = True
-		rev_joints[rj[1]].jointMotion.rotationValue = user_joint_angles[rj[1]] + dtheta
+# def exploreLocation(rigid_joints, rev_joints):
+# 	#supporess one rigid joint then explore dtheta/ddist in both directions around that point
+# 	#the repeat for the other two joints
+# 	dthetas
+# 	dtheta = 0.0001#rad
+# 	for rj in zip(rigid_joints, list(range(3))):
+# 		rj[0].isSuppressed = True
+# 		rev_joints[rj[1]].jointMotion.rotationValue = user_joint_angles[rj[1]] + dtheta
 
 
 
@@ -165,8 +175,32 @@ def run(context):
 		joint2_rev = rootComp.joints.itemByName('Shoulder2_Revolute')
 
 		rigid_joints = [rigid0, rigid1, rigid2]
-		rev_joints = [joint0_rev, joint1_rev, joint2_rev]
+		revolute_joints = [joint0_rev, joint1_rev, joint2_rev]
 
+		dtheta = 0.1*pi/180.0#0.1degree
+
+		xyzs = []
+		thetas = []
+		dthetas = []
+		dxyzs = []
+		for t1 in range(-3,3):
+			for t2 in range(-3,3):
+				for t3 in range(-3,3):
+					#set the angles
+					setRigidJoints(rigid_joints, revolute_joints, [6*t1*pi/180, 6*t2*pi/180, 6*t2*pi/180])
+					#calc mobile platform position
+					cur_xyzs = [mobilePlatform.transform.translation.x, mobilePlatform.transform.translation.y, mobilePlatform.transform.translation.x]
+					xyzs.append(cur_xyzs)
+					thetas.append([t1,t2,t3])
+					#now move by dtheta in each axis
+					setRigidJoints(rigid_joints, revolute_joints, [20*t1*pi/180+dtheta, 20*t2*pi/180+dtheta, 20*t2*pi/180+dtheta])
+					nxyzs = [mobilePlatform.transform.translation.x, mobilePlatform.transform.translation.y, mobilePlatform.transform.translation.x]
+					dxyzs.append(list(map(operator.sub, nxyzs, cur_xyzs)))
+					dthetas.append(dtheta)
+
+
+
+		###########TEST###########
 		# test_len = 100
 		# thetas = [[pi*random.randint(-50,50)/180.0, pi*random.randint(-50,50)/180.0, pi*random.randint(-50,50)/180.0 ] for k in range(test_len)]
 		# start_time = time.time()
@@ -175,15 +209,21 @@ def run(context):
 		# stop_time = time.time()
 
 		# delta_time = stop_time-start_time
+		###########TEST###########
 
 
 
+		###########TEST###########
+		# start_time = time.time()
+		# thetas, xyzs = sweepJoint(0, rigid_joints, rev_joints, [k*pi/180 for k in range(-50,50)], mobilePlatform)
+		# stop_time = time.time()
+		###########TEST###########
 
-		start_time = time.time()
-		thetas, xyzs = sweepJoint(0, rigid_joints, rev_joints, [k*pi/180 for k in range(-50,50)], mobilePlatform)
-		stop_time = time.time()
+		mydata={'thetas':thetas, 'xyzs':xyzs, 'dthetas':dthetas, 'dxyzs':dxyzs}
 
+		write2json(jsonFullPath, mydata)
 
+#		forCSV = [xyzs, dxyzs, dthetas]
 
 		write2CSV(thetas, fullPath)
 
