e26421cce5b5470835f0c71d6deb726be46d5b4f
Marc Grossman <marcgrossman@Marcs-MacBook-Pro.local>
2018-06-28 13:55:44 -0700

    The sweep is working well, and quickly. The set rigid joints is very slow. 5s per. Need to fix. No longer using my class, but would like to re-implement and incorporate it

--- delta/ROMplot.py
+++ delta/ROMplot.py
@@ -3,6 +3,7 @@ import os
 import plotly.plotly as py
 import plotly.graph_objs as go
 import numpy as np
+import json
 
 import pdb
 
@@ -19,16 +20,26 @@ def readCSV(fullPath):
 	return blah
 
 
+def import_json(fullPath):
+	with open(fullPath) as fp:
+		data = json.load(fp)
+
+	return data
+
 
 
 if __name__ == '__main__':
-	fullPath = os.path.join(os.path.expanduser('~'), 'Documents','Output4.csv')
+	# fullPath = os.path.join(os.path.expanduser('~'), 'Documents','Output4.csv')
+	fullPath = os.path.join(os.path.expanduser('~'), 'Documents','data5.json')
 
-	blah = readCSV(fullPath)
+	# blah = readCSV(fullPath)
+	data = import_json(fullPath)
 	# pdb.set_trace()
-	xyzs = [eval(k[1]) for k in blah]
-	thetas = [eval(k[0]) for k in blah]
-	# pdb.set_trace()
+	xyzs = data['xyzs']
+	thetas = data['thetas']
+	# xyzs = [eval(k[1]) for k in blah]
+	# thetas = [eval(k[0]) for k in blah]
+	pdb.set_trace()
 
 	# x2, y2, z2 = np.random.multivariate_normal(np.array([0,0,0]), np.eye(3), 200).transpose()
 	trace0 = go.Scatter3d(
