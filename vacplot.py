f0f491e3fcafe011797746c55c2c08e55900570f
grossman83 <marc.a.grossman@gmail.com>
2021-12-07 21:39:33 -0800

    adding initial version of vacplot as received from Cedric on 7DEC2021

---
+++ vacplot.py
@@ -0,0 +1,206 @@
+import argparse
+import json
+import pprint as pp
+import sys
+import traceback
+import time
+
+import matplotlib.pyplot as plt
+import numpy as np
+import serial
+from serial.tools import list_ports
+
+from scipy.optimize import minimize
+
+TEST_VOLUME = 1.75  # Liter
+
+ATM_PRESS = 100.0  #kPa
+IDEAL_GAS = 22.4  #L/mol
+TEST_TEMPERATURE = 293.0  #Kelvin
+STD_TEMPERATURE = 273.15  #Kelvin
+
+
+def model_pressure(flow_coeff, volume, time, max_delta_P):
+	mols = volume / (IDEAL_GAS * TEST_TEMPERATURE / STD_TEMPERATURE)
+	press = max_delta_P * (1 - np.exp(-time / (flow_coeff * mols)))
+	return press
+
+
+def process_flowrate_data(volume, time, delta_P):
+	""" expects timeseries data
+		time is an iterable of time data in seconds
+		delta_P is an iterable same size as time in kPa
+		from a vacuum test (evacuating a known volume)
+		The evacuation of a fixed volume chamber using a pump closely
+		resembles the charge/discharge of a capacitor
+
+		V = V(1-e^(-t/RC))
+		I = (V/R)*e^(-t/RC)
+
+		Potential (V) is analogous to Pressure (kPa)
+		Resistance (R) is now in units of seconds/mol
+		Capacitance (C) is in units of mols
+
+		Current (I) is analogous to rate of change of pressure (kPa/s)
+
+		Gas law: PV=nrT
+
+		We can determine Max flow in Liters/second by converting s/mol,
+		using the gas law and 22.4L/mol ideal gas at STP.
+
+		Using pressure samples over time from the evacuation of a known
+		volume we can determine the Resistance (R) or flow coefficient
+		of the pump, and from the asymptote of the pressure we can
+		determine the total potential of the pump (V)
+	"""
+
+	#a simple least squares residual objective function
+	def obj_fcn(flow_coeff, volume, max_delta_P, time, delta_P):
+
+		modeled_press = np.array([
+			model_pressure(flow_coeff, volume, t, max_delta_P) for t in time
+		])
+		modeled_press = modeled_press.flatten()
+		delta_P = np.array(delta_P)
+
+		res = np.sqrt(np.sum((delta_P - modeled_press)**2))
+
+		return res
+
+	#maximum delta P (just use the last few data points)
+	max_delta_P = np.median(delta_P[-5:])
+
+	result = minimize(
+		obj_fcn,
+		1.0,
+		args=(volume, max_delta_P, time, delta_P),
+		method='Nelder-Mead',
+		options={'maxiter':1000, 'disp':True}
+	)
+
+	flow_coeff = result['x'][0]
+
+	max_flow = (IDEAL_GAS * TEST_TEMPERATURE / STD_TEMPERATURE) / flow_coeff
+
+	return {
+		'flow_coeff': flow_coeff,
+		'max_delta_P': max_delta_P,
+		'max_flow': max_flow
+	}
+
+
+def plot_vac_data(ts, delta_P, modeled_press=None):
+	""" Generate a plot of the
+	"""
+
+	fig = plt.figure()
+	ax = fig.add_subplot(1, 1, 1)
+
+	handles = ax.plot(ts, delta_P, '.b', label="experimental")
+
+	if modeled_press is not None:
+		h2 = ax.plot(ts, modeled_press, '-r', label="model")
+		handles.extend(h2)
+
+	ax.set_title("Vacuum Data Plot")
+	ax.set_xlabel("Time (S)")
+	ax.set_ylabel("Differential Pressure (kPa)")
+	ax.legend(handles=handles)
+	return fig
+
+
+def setup_opts():
+	parser = argparse.ArgumentParser(description="Vacuum Plot")
+
+	parser.add_argument(
+		"-l",
+		"--list",
+		action='store_true',
+		required=False,
+		help="List available ports"
+	)
+
+	parser.add_argument(
+		"-p",
+		"--port",
+		type=str,
+		required=False,
+		help="COM port for pressure sensor data eg: /dev/ttyACM0"
+	)
+
+	opts = parser.parse_args()
+
+	return opts
+
+
+if __name__ == "__main__":
+
+	opts = setup_opts()
+
+	ports = list_ports.comports()
+
+	if opts.list:
+		print("Available COM ports:")
+		pp.pprint([x.device for x in ports])
+		sys.exit(0)
+
+	if opts.port is None:
+		if len(ports) == 0:
+			print("No COM port detected")
+			sys.exit(1)
+		elif len(ports) == 1:
+			# try to use the only available port
+			opts.port = ports[0].device
+		else:
+			print("Please select a port using --port option:")
+			pp.pprint([x.name for x in ports])
+			sys.exit(1)
+
+	print("Connecting to: {}".format(opts.port))
+
+	#connect to the comport to collect data
+	ser = serial.Serial(opts.port, 9600)
+	data = ser.readline(1000) # check that we get something from the port
+	if data is None:
+		print("No data rxed...")
+		sys.exit(1)
+
+	data = []
+	start_ts = None
+	TIMEOUT = 3.0 #seconds after start detected
+	MIN_TRESH = 1.0
+	while True:
+		press = float(ser.readline(1000).decode('utf-8'))
+		ts = time.time()
+
+		# check start condition
+		if start_ts is None and press > MIN_TRESH:
+			print("Start condition")
+			start_ts = ts
+
+		# check timeout condition
+		if start_ts is not None:
+			if ts - start_ts > TIMEOUT:
+				break
+
+		#record and print data
+		if start_ts is not None:
+			data.append((ts, press))
+			print("{}, {}".format(ts, press))
+
+
+	print("Processing Results...")
+	ts, delta_P = zip(*data)
+	result = process_flowrate_data(TEST_VOLUME, ts, delta_P)
+	pp.pprint(result)
+
+	#generate the model fit points
+	flow_coeff = result['flow_coeff']
+	max_delta_P = result['max_delta_P']
+	modeled_press = np.array([
+		model_pressure(flow_coeff, TEST_VOLUME, t, max_delta_P) for t in ts
+	])
+
+	fig = plot_vac_data(ts, delta_P, modeled_press)
+
+	plt.show(block=True)
