5303d8b08a8ed53881e809305a227d355258a13d
MARC GROSSMAN <marc@MARCs-MBP-2.attlocal.net>
2021-12-22 19:36:07 -0800

    initial

---
+++ vacplot_promicro/vacplot_promicro.ino
@@ -0,0 +1,29 @@
+#include <RingBuf.h>
+
+
+void setup() {
+  // start serial port at 115200 bps and wait for port to open:
+  Serial.begin(115200);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+}
+
+unsigned int p = 0;
+unsigned int ts = 0;
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  p = analogRead(A0); //yields 10 bits of useful information
+  while (p > 920) {
+    delay(1);
+    p = analogRead(A0);
+  }
+  ts = millis();
+  Serial.print(p);
+  Serial.print(", ");
+  Serial.print(ts);
+  Serial.print('\n');
+  delay(2);
+
+}
