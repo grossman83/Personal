5b0bf90fdfa7ceb6e003f7e906dd2f5c5369cf12
grossman83 <marc.a.grossman@gmail.com>
2022-02-01 09:57:02 -0800

    Removed the necessity to be connected to a computer (serial) Setup logger to log 10,000 points every time button is pressed then it waits in a state where button can be pressed again This could be modified with a timer to cause it to log 10,000 points every x seconds minutes or whatever.

--- vibration/teensylogger-buffered/teensylogger-buffered.ino
+++ vibration/teensylogger-buffered/teensylogger-buffered.ino
@@ -1,5 +1,6 @@
-#include <SD_t3.h>
+//#include <SD_t3.h>
 #include <SD.h>
+//#include "home/marc/arduino-1.8.19/hardware/teensy/avr/libraries/SD/src/SD.h"
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
 #include <Wire.h>
@@ -10,9 +11,12 @@
 // Reference Material
 //https://github.com/rlogiacco/CircularBuffer
 
-char filename[] = "I1800.CSV";
+char filename[] = "31JAN2021 1902PDT.CSV";
 const int btn = 23; //btn pin
 int btnState = 1;
+
+const bool serialconn = 0;
+
 //macro for detection af rasing edge
 #define RE(signal, state) (state=(state<<1)|(signal&1)&3)==1
 #define FE(signal, state) (state=(state<<1)|(signal&1)&3)==2
@@ -30,87 +34,107 @@ int led_pin = 13;
 Adafruit_MPU6050 mpu;
 
 void setup(void) {
-  Serial.begin(115200);
-  while (!Serial)
-    delay(10); // will pause Zero, Leonardo, etc until serial console opens
-
-  Serial.println("Adafruit MPU6050 test!");
-
-  // Try to initialize!
-  if (!mpu.begin()) {
-    Serial.println("Failed to find MPU6050 chip");
-    while (1) {
-      delay(1000);
+  if (serialconn) {
+    Serial.begin(115200);
+    while (!Serial)
+      delay(10); // will pause Zero, Leonardo, etc until serial console opens
+  
+    Serial.println("Adafruit MPU6050 test!");
+  
+    // Try to initialize!
+    if (!mpu.begin()) {
       Serial.println("Failed to find MPU6050 chip");
+      while (1) {
+        delay(1000);
+        Serial.println("Failed to find MPU6050 chip");
+      }
+    }
+    Serial.println("MPU6050 Found!");
+  }
+
+  pinMode(led_pin, OUTPUT);
+  pinMode(btn, INPUT_PULLUP);
+  if (!mpu.begin()){
+    while(1){
+      digitalWrite(led_pin, 0);
+      delay(75);
+      digitalWrite(led_pin, 1);
+      delay(75);      
     }
   }
-  Serial.println("MPU6050 Found!");
-
   mpu.setAccelerometerRange(MPU6050_RANGE_2_G);
-  Serial.print("Accelerometer range set to: ");
-  switch (mpu.getAccelerometerRange()) {
-  case MPU6050_RANGE_2_G:
-    Serial.println("+-2G");
-    break;
-  case MPU6050_RANGE_4_G:
-    Serial.println("+-4G");
-    break;
-  case MPU6050_RANGE_8_G:
-    Serial.println("+-8G");
-    break;
-  case MPU6050_RANGE_16_G:
-    Serial.println("+-16G");
-    break;
+  if (serialconn) {
+    Serial.print("Accelerometer range set to: ");
+    switch (mpu.getAccelerometerRange()) {
+    case MPU6050_RANGE_2_G:
+      Serial.println("+-2G");
+      break;
+    case MPU6050_RANGE_4_G:
+      Serial.println("+-4G");
+      break;
+    case MPU6050_RANGE_8_G:
+      Serial.println("+-8G");
+      break;
+    case MPU6050_RANGE_16_G:
+      Serial.println("+-16G");
+      break;
+    }
   }
   mpu.setGyroRange(MPU6050_RANGE_500_DEG);
-  Serial.print("Gyro range set to: ");
-  switch (mpu.getGyroRange()) {
-  case MPU6050_RANGE_250_DEG:
-    Serial.println("+- 250 deg/s");
-    break;
-  case MPU6050_RANGE_500_DEG:
-    Serial.println("+- 500 deg/s");
-    break;
-  case MPU6050_RANGE_1000_DEG:
-    Serial.println("+- 1000 deg/s");
-    break;
-  case MPU6050_RANGE_2000_DEG:
-    Serial.println("+- 2000 deg/s");
-    break;
+  if (serialconn) {
+    Serial.print("Gyro range set to: ");
+    switch (mpu.getGyroRange()) {
+    case MPU6050_RANGE_250_DEG:
+      Serial.println("+- 250 deg/s");
+      break;
+    case MPU6050_RANGE_500_DEG:
+      Serial.println("+- 500 deg/s");
+      break;
+    case MPU6050_RANGE_1000_DEG:
+      Serial.println("+- 1000 deg/s");
+      break;
+    case MPU6050_RANGE_2000_DEG:
+      Serial.println("+- 2000 deg/s");
+      break;
+    }
   }
 
   mpu.setFilterBandwidth(MPU6050_BAND_260_HZ);
-  Serial.print("Filter bandwidth set to: ");
-  switch (mpu.getFilterBandwidth()) {
-  case MPU6050_BAND_260_HZ:
-    Serial.println("260 Hz");
-    break;
-  case MPU6050_BAND_184_HZ:
-    Serial.println("184 Hz");
-    break;
-  case MPU6050_BAND_94_HZ:
-    Serial.println("94 Hz");
-    break;
-  case MPU6050_BAND_44_HZ:
-    Serial.println("44 Hz");
-    break;
-  case MPU6050_BAND_21_HZ:
-    Serial.println("21 Hz");
-    break;
-  case MPU6050_BAND_10_HZ:
-    Serial.println("10 Hz");
-    break;
-  case MPU6050_BAND_5_HZ:
-    Serial.println("5 Hz");
-    break;
+  if (serialconn){
+    Serial.print("Filter bandwidth set to: ");
+    switch (mpu.getFilterBandwidth()) {
+    case MPU6050_BAND_260_HZ:
+      Serial.println("260 Hz");
+      break;
+    case MPU6050_BAND_184_HZ:
+      Serial.println("184 Hz");
+      break;
+    case MPU6050_BAND_94_HZ:
+      Serial.println("94 Hz");
+      break;
+    case MPU6050_BAND_44_HZ:
+      Serial.println("44 Hz");
+      break;
+    case MPU6050_BAND_21_HZ:
+      Serial.println("21 Hz");
+      break;
+    case MPU6050_BAND_10_HZ:
+      Serial.println("10 Hz");
+      break;
+    case MPU6050_BAND_5_HZ:
+      Serial.println("5 Hz");
+      break;
+    }
   }
 
 
   //SD CARD STUFF
   delay(100);
-  Serial.print("\nInitializing SD card...");
+  if (serialconn) {
+    Serial.print("\nInitializing SD card...");
+  }
   bool blah = SD.begin(BUILTIN_SDCARD);
-  if (blah) {
+  if (blah && (serialconn)) {
     Serial.println("SD Begin Success");
   }
 
@@ -125,7 +149,9 @@ void record_data(){
   int record = 1;
   File dataFile = SD.open(filename, FILE_WRITE);
   if (dataFile) {
-    Serial.println("Data File Opened");
+    if (serialconn) {
+      Serial.println("Data File Opened");
+    }
     digitalWrite(led_pin, record);
     int count = 0;
     int ts = 0;
@@ -158,7 +184,9 @@ void record_data(){
         
         dataFile.close();
         record = 0;
-        Serial.println("Data File Closed");
+        if (serialconn) {
+          Serial.println("Data File Closed");
+        }
         digitalWrite(led_pin, 0);
         delay(100);
         break;
@@ -166,7 +194,9 @@ void record_data(){
     }
   }
   else {
-    Serial.println("Unable to open SD card");
+    if (serialconn) {
+      Serial.println("Unable to open SD card");
+    }
   }
 }
 
@@ -175,11 +205,20 @@ void record_data(){
 
 void loop() {
   delay(100);
-  Serial.println("in loop");
+  if (serialconn) {
+    Serial.println("in loop");
+  }
   if(!digitalRead(btn)){
-    Serial.println("recording");
-    delay(300);
+    if (serialconn) {
+      Serial.println("recording");
+    }
     record_data();
+  }
+  for (int k = 0; k<10; k++){
+    delay(500);
+    digitalWrite(led_pin, 0);
+    delay(500);
+    digitalWrite(led_pin, 1);
+  }
     
-  } 
 }
