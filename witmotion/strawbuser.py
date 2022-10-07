738e57384d341bf1284769058c1edbf807050766
MARC GROSSMAN <marc@MARCs-MacBook-Pro-2.local>
2022-10-06 17:28:40 -0700

    working now in that it exports a transparent gif at 15 fps the gif shows the acceleration throughout, but then overlays red dots on it up to the time in the video. Need to do some time syncing stuff... particularly because i'm not sure gifs can even have decimal frame rates.

--- witmotion/strawbuser.py
+++ witmotion/strawbuser.py
@@ -1,6 +1,7 @@
 import numpy as np
 import csv
 from matplotlib import pyplot as plt
+from matplotlib.animation import PillowWriter
 import sys
 import os
 import pdb
@@ -82,7 +83,7 @@ if __name__ =='__main__':
 		#experienced by the test berry.
 		rate = 44100 #samples per second
 		T = rec_time.seconds + rec_time.microseconds / 1000000
-		f = 1000.0 # sound frequency [Hz]
+		f = 400.0 # sound frequency [Hz]
 		t = np.linspace(0, T, int(T*rate))
 		x=0.3 * np.sin(2*np.pi*f*t)
 
@@ -100,21 +101,62 @@ if __name__ =='__main__':
 
 
 
-		fig = plt.figure(figsize=(18,10))
+		fig = plt.figure(figsize=(12,8))
 		ax = fig.add_subplot(1,1,1)
 		ax.set_title("Acceleration vs Time")
 		ax.set_xlabel("Time [s]")
+		ax.set_ylim([0,30])
+		# plt.axes('off')
 		ax.set_ylabel("RMS Acceleration [g]")
-		ax.plot(zero_start_times, a_rms)
-		ax.grid(axis='x', which='major')
-		ax.grid(axis='x', which='minor')
-		plt.savefig(date_str + ".png")
-		# plt.show()
-		# ax.plot(t, mixed)
-
-
-
+		ax.plot(zero_start_times, a_rms, '-b')
+		# ax.grid(axis='x', which='major')
+		# ax.grid(axis='x', which='minor')
+		# plt.ion()
+		# plt.vlines(25, 0, 25, colors = 'r', linestyles='dotted')
 		# pdb.set_trace()
+		plt.savefig(date_str + ".png", transparent = True)
+
+
+		#now try to make an animated plot that highlights the part of the plot
+		#corresponding to the amplitude of the acceleration of the berry
+		#as shown on the screen.
+
+		# afig, ax = plt.subplots()
+		# line, = plt.plot([0,1])
+
+		# def animate(i):
+		# 	line.set_ydata
+
+
+
+
+		# creating gif so I can overlay it with the video
+		metadata = dict(title='Movie', artist='Marc')
+		writer = PillowWriter(fps=15, metadata=metadata)
+		xlist = []
+		ylist = []
+
+
+		with writer.saving(fig, 'blah.gif', 100):
+			for xval in range(300):
+				# l.set_data(zero_start_times[0:xval], a_rms[0:xval].tolist())
+				# plt.clf()
+				# ax.set_ylim([0,20])
+				# plt.bar(0, a_rms[xval], color='b', width=0.5, bottom=0, align='center')
+				# plt.axis([0,1,0,20])
+				# ax.set_axis_off()
+				# pdb.set_trace()
+				# plt.axes('off')
+				#line = plt.vlines(50*(xval+1)/1000.0, 0, 30, colors = 'r', linestyles='dotted')
+				ax.plot([zero_start_times[20*xval]], [a_rms[20*xval]], '.r')
+				writer.grab_frame(transparent = True)
+				# pdb.set_trace()
+				# line.remove()
+				# newline = line = plt.vlines(50*xval/1000.0, 0, 30, colors = '', linestyles='dotted')
+				# plt.axes('on')
+
+
+		pdb.set_trace()
 
 		wavio.write(date_str + ".wav", mixed, rate, sampwidth=3)
 
