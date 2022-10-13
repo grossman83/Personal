3211f3e7ca0f9080df1ee3dfabbd374478b17b8b
MARC GROSSMAN <marc@MARCs-MacBook-Pro-2.local>
2022-10-13 12:05:50 -0700

    Added the ability to do a window filter of arbitrary length on both the accelerometer and gyro data.
    Cleaned up some old unused code related to creating the gifs.
    
    Cedric wants to imrpove it to show the data frame by frame in a
    way that matches the framerate of the video. I told him this
    caused the computer to bog down. He thinks I'm not handling things
    correctly.
    
    need to do some plt.cla()
    ax.show() etc.

--- witmotion/strawbuser.py
+++ witmotion/strawbuser.py
@@ -31,21 +31,24 @@ if __name__ =='__main__':
 		beta = []
 		gamma = []
 		for row in spamreader:
-			timestamp.append(datetime.strptime(row[0][0:-2], '%Y-%m-%d %H:%M:%S'))
-			decimals.append(np.int(row[0][-1]))
-			ax.append(float(row[2]))
-			ay.append(float(row[3]))
-			az.append(float(row[4]))
+			# pdb.set_trace()
+			if(len(row) > 5):
+				timestamp.append(datetime.strptime(row[0][0:-2], '%Y-%m-%d %H:%M:%S'))
+				decimals.append(np.int(row[0][-1]))
+				az.append(float(row[4]))
+				ay.append(float(row[3]))
+				ax.append(float(row[2]))
 
-			try:
-				gamma.append(float(row[16]))
-				beta.append(float(row[15]))
-				alpha.append(float(row[14]))
-				# w_timestamp.append(datetime.strptime(row[0][0:-2], '%Y-%m-%d %H:%M:%S'))
-			except:
-				gamma.append(np.nan)
-				beta.append(np.nan)
-				alpha.append(np.nan)
+				try:
+					gamma.append(float(row[16]))
+					beta.append(float(row[15]))
+					alpha.append(float(row[14]))
+					
+					# w_timestamp.append(datetime.strptime(row[0][0:-2], '%Y-%m-%d %H:%M:%S'))
+				except:
+					gamma.append(np.nan)
+					beta.append(np.nan)
+					alpha.append(np.nan)
 
 		ax = np.array(ax)
 		ay = np.array(ay)
@@ -94,6 +97,7 @@ if __name__ =='__main__':
 		zero_based_times = [k.seconds + k.microseconds/1000000 for k in zero_ts]
 
 		a_rms = np.sqrt(ax**2 + ay**2 + az**2)
+		# a_rms = np.convolve(a_rms, np.ones(5)/5, mode='same')
 		w_rms = np.sqrt(alpha**2 + beta**2 + gamma**2) * np.pi/180.
 		w_rms = np.convolve(w_rms, np.ones(100)/100, mode='same')
 		rec_time = np.max(zero_based_times)
@@ -116,8 +120,9 @@ if __name__ =='__main__':
 		mixed = a_audio * x
 		wavio.write(date_str + ".wav", mixed, rate, sampwidth=3)
 
-
-		fig = plt.figure(figsize=(16,12))
+		# pdb.set_trace()
+		plt.ion()
+		fig = plt.figure(figsize=(16,9))
 		ax = fig.add_subplot(1,1,1)
 		ax.set_title("Acceleration vs Time")
 		ax.set_ylim([0,30])
@@ -133,17 +138,21 @@ if __name__ =='__main__':
 		ax.plot(zero_start_times, a_rms, '-b', markersize=1)
 		ax.plot(zero_start_times, w_rms, '.c', markersize=2)
 		plt.savefig(date_str + ".png", transparent = False)
-		plt.ion()
-		plt.show()
+
+		# plt.show()
+		# pdb.set_trace()
 
 
 
-		video_duration = np.max(zero_start_times)
-		fps = 30
-		num_frames = int(np.round(video_duration*fps))
-		pts_per_frame = len(a_rms)/num_frames
-		pts_per_second = fps*pts_per_frame
+
+		# video_duration = np.max(zero_start_times)
+		# fps = 30
+		# num_frames = int(np.round(video_duration*fps))
+		# pts_per_frame = len(a_rms)/num_frames
+		# pts_per_second = fps*pts_per_frame
 		fps=1
+
+		# pdb.set_trace()
 
 		# creating gif so I can overlay it with the video
 		metadata = dict(title='Movie', artist='Marc')
@@ -155,6 +164,8 @@ if __name__ =='__main__':
 				writer.grab_frame(transparent = True)
 				scatter = ax.plot([xval], [0], '.g', markersize=10)
 				print(xval/max(zero_start_times))
+				# plt.show()
+				# plt.cla()
 
 		
 
