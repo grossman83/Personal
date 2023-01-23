7560a059af4e77fbc4c6f0868c4dd88d9f8ee879
MARC GROSSMAN <marc@MARCs-MacBook-Pro-2.local>
2023-01-23 11:30:45 -0800

    added testing.py when dropped off at core-care

---
+++ ip/testing.py
@@ -0,0 +1,23 @@
+import sys, os
+import numpy as np
+import cv2
+import pdb
+import matplotlib.pyplot as plt
+
+
+if __name__ =='__main__':
+	
+	
+
+	#create an array of 50x50x3 between 0 and 255
+	blah = np.random.randint(0,255, (100,100,3), dtype=np.uint8)
+
+
+	pdb.set_trace()
+
+	window_name = 'image'
+	cv2.imshow(window_name, blah)
+	cv2.waitKey(0)
+	cv2.destroyAllWindows()
+
+	pdb.set_trace()
