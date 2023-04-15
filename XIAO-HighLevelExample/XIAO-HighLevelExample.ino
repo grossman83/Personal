bb4db25aa8fda6d2ab6c5efaf6855b5397ffca6f
MARC GROSSMAN <marc@MARCs-2018-MacBook-Pro.local>
2023-04-15 15:00:00 -0700

    working IMU printing to serial.

--- XIAO-HighLevelExample/XIAO-HighLevelExample.ino
+++ XIAO-HighLevelExample/XIAO-HighLevelExample.ino
@@ -1,5 +1,4 @@
 #include <LSM6DS3.h>
-
 // #include "/Users/marc/Documents/Arduino/libraries/Seeed_Arduino_LSM6DS3/LSM6DS3.h"
 #include "Wire.h"
 
@@ -10,14 +9,18 @@
 LSM6DS3 myIMU(I2C_MODE, LSM6DS3_ADDRESS);    //I2C device address 0x6A
 // LSM6DS3 myIMU();    //I2C device address 0x6A
 
-float x = 0;
-float y = 0;
-float z = 0;
-int a = 0;
+// float x = 0;
+// float y = 0;
+// float z = 0;
+// int a = 0;
+
+
+// const int analogInPin = A0;  // Analog input pin that the potentiometer is attached to
+int sensorValue = 0;  // value read from the pot
 
 void setup() {
     // put your setup code here, to run once:
-    Serial.begin(9600);
+    Serial.begin(115200);
     while (!Serial);
     //Call .begin() to configure the IMUs
     if (myIMU.begin() != 0) {
@@ -29,30 +32,32 @@ void setup() {
 
 void loop() {
     //Accelerometer
-    x = myIMU.readFloatAccelX();
-    Serial.print("\nAccelerometer:\n");
-    Serial.print(" X1 = ");
+    // Serial.print("\nAccelerometer:\n");
+    // Serial.print(" X1 = ");
     Serial.println(myIMU.readFloatAccelX(), 4);
-    Serial.print(" Y1 = ");
+    // Serial.print(" Y1 = ");
     Serial.println(myIMU.readFloatAccelY(), 4);
-    Serial.print(" Z1 = ");
+    // Serial.print(" Z1 = ");
     Serial.println(myIMU.readFloatAccelZ(), 4);
+    sensorValue = analogRead(A0);
+    Serial.println(sensorValue);
+    Serial.print("\n");
 
     //Gyroscope
-    Serial.print("\nGyroscope:\n");
-    Serial.print(" X1 = ");
-    Serial.println(myIMU.readFloatGyroX(), 4);
-    Serial.print(" Y1 = ");
-    Serial.println(myIMU.readFloatGyroY(), 4);
-    Serial.print(" Z1 = ");
-    Serial.println(myIMU.readFloatGyroZ(), 4);
+    // Serial.print("\nGyroscope:\n");
+    // Serial.print(" X1 = ");
+    // Serial.println(myIMU.readFloatGyroX(), 4);
+    // Serial.print(" Y1 = ");
+    // Serial.println(myIMU.readFloatGyroY(), 4);
+    // Serial.print(" Z1 = ");
+    // Serial.println(myIMU.readFloatGyroZ(), 4);
 
     //Thermometer
-    Serial.print("\nThermometer:\n");
-    Serial.print(" Degrees C1 = ");
-    Serial.println(myIMU.readTempC(), 4);
-    Serial.print(" Degrees F1 = ");
-    Serial.println(myIMU.readTempF(), 4);
+    // Serial.print("\nThermometer:\n");
+    // Serial.print(" Degrees C1 = ");
+    // Serial.println(myIMU.readTempC(), 4);
+    // Serial.print(" Degrees F1 = ");
+    // Serial.println(myIMU.readTempF(), 4);
 
-    delay(100);
+    delay(10);
 }
