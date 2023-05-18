2cfc597895d995a0e62ea4760407c07df80bf631
MARC GROSSMAN <marc@MARCs-2018-MacBook-Pro.local>
2023-05-18 15:22:26 -0700

    working correctly.
    Sends accelerometer, gyroscope, and temp data to the serial
    monitor built into arduino.

--- XIAO-HighLevelExample/XIAO-HighLevelExample.ino
+++ XIAO-HighLevelExample/XIAO-HighLevelExample.ino
@@ -5,9 +5,7 @@
 
 #define LSM6DS3_ADDRESS 0x6A
 
-// TwoWire myWire = TwoWire(0);  // Use Wire1 for I2C communication
 LSM6DS3 myIMU(I2C_MODE, LSM6DS3_ADDRESS);    //I2C device address 0x6A
-// LSM6DS3 myIMU();    //I2C device address 0x6A
 
 // float x = 0;
 // float y = 0;
@@ -45,31 +43,33 @@ void setup() {
 
 void loop() {
     //Accelerometer
-    // Serial.print("\nAccelerometer:\n");
-    // Serial.print(" X1 = ");
-    // Serial.println(myIMU.readFloatAccelX(), 4);
-    // Serial.print(" Y1 = ");
-    // Serial.println(myIMU.readFloatAccelY(), 4);
-    // Serial.print(" Z1 = ");
-    // Serial.println(myIMU.readFloatAccelZ(), 4);
-    sensorValue = analogRead(A0);
-    Serial.println(sensorValue);
+    Serial.print("\nAccelerometer:\n");
+    Serial.print(" X1 = ");
+    Serial.println(myIMU.readFloatAccelX(), 4);
+    Serial.print(" Y1 = ");
+    Serial.println(myIMU.readFloatAccelY(), 4);
+    Serial.print(" Z1 = ");
+    Serial.println(myIMU.readFloatAccelZ(), 4);
+    
+    
+    // sensorValue = analogRead(A0);
+    // Serial.println(sensorValue);
 
-    //Gyroscope
-    // Serial.print("\nGyroscope:\n");
-    // Serial.print(" X1 = ");
-    // Serial.println(myIMU.readFloatGyroX(), 4);
-    // Serial.print(" Y1 = ");
-    // Serial.println(myIMU.readFloatGyroY(), 4);
-    // Serial.print(" Z1 = ");
-    // Serial.println(myIMU.readFloatGyroZ(), 4);
+    // Gyroscope
+    Serial.print("\nGyroscope:\n");
+    Serial.print(" X1 = ");
+    Serial.println(myIMU.readFloatGyroX(), 4);
+    Serial.print(" Y1 = ");
+    Serial.println(myIMU.readFloatGyroY(), 4);
+    Serial.print(" Z1 = ");
+    Serial.println(myIMU.readFloatGyroZ(), 4);
 
-    //Thermometer
-    // Serial.print("\nThermometer:\n");
-    // Serial.print(" Degrees C1 = ");
-    // Serial.println(myIMU.readTempC(), 4);
-    // Serial.print(" Degrees F1 = ");
-    // Serial.println(myIMU.readTempF(), 4);
+    // Thermometer
+    Serial.print("\nThermometer:\n");
+    Serial.print(" Degrees C1 = ");
+    Serial.println(myIMU.readTempC(), 4);
+    Serial.print(" Degrees F1 = ");
+    Serial.println(myIMU.readTempF(), 4);
 
     // delay(100);
     // analogWrite(LED_BUILTIN, 0);
