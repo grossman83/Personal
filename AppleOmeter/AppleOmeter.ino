1c03c15f24c103a58eda5e9daf6ccdd7158c5feb
MARC GROSSMAN <marc@marcbookair.local>
2024-04-20 15:06:31 -0700

    It's now working with deep sleep function and screen. There is still some remaining work to do... * add control of the power to the screen with a FET * show the highest accel over the last minute or more not sure what max is

--- AppleOmeter/AppleOmeter.ino
+++ AppleOmeter/AppleOmeter.ino
@@ -1,8 +1,12 @@
 #include <LSM6DS3.h>
-// #include "/Users/marc/Documents/Arduino/libraries/Seeed_Arduino_LSM6DS3/LSM6DS3.h"
 #include "Wire.h"
-
 #include "CircularBuffer.hpp"
+
+//low power sleep stuff
+#include "Adafruit_SPIFlash.h"
+#define DO_WORK_PIN   D1
+#define SHUTDOWN_PIN  D2
+
 
 //Display Stuff
 #include <Adafruit_GFX.h>
@@ -11,18 +15,27 @@
 #define SCREEN_HEIGHT 64
 #define OLED_RESET    -1
 #define D0 0
+
+
+
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
-
-
-
 #define LSM6DS3_ADDRESS 0x6A
-
 LSM6DS3 myIMU(I2C_MODE, LSM6DS3_ADDRESS);    //I2C device address 0x6A
 
+// More sleep stuff
+Adafruit_FlashTransport_QSPI flashTransport;
+SemaphoreHandle_t xSemaphore;
+bool gotoSystemOffSleep = false;
+int work_LED_status = HIGH;
+int boot_millis = 0;
 
-int LEDR = 11;
-int LEDB = 12;
-int LEDG = 13;
+void QSPIF_sleep(void)
+{
+  flashTransport.begin();
+  flashTransport.runCommand(0xB9);
+  flashTransport.end();  
+}
+
 
 
 // const int analogInPin = A0;  // Analog input pin that the potentiometer is attached to
@@ -43,8 +56,6 @@ CircularBuffer<uint8_t, slow_buf_length> slow_buffer;
 
 void setup() {
     // put your setup code here, to run once:
-
-
     //########################Serial is for debugging only#############
     /*
     Serial.begin(115200);
@@ -57,6 +68,10 @@ void setup() {
     }
     */
     //########################Serial is for debugging only#############
+
+
+
+
 
     // myIMU.settings.accelFifoDecimation = 0x06;//decimation by a factor of 4
     myIMU.settings.accelSampleRate = 416;
@@ -90,20 +105,54 @@ void setup() {
     //########################DISPLAY##############################
 
 
-    pinMode(LEDR, OUTPUT);
-    pinMode(LEDG, OUTPUT);
-    pinMode(LEDB, OUTPUT);
     pinMode(LED_BUILTIN, OUTPUT);
 
     // set the resolution of the analog read to 12 bits. 
     // analogReadResolution(12);
 
 
+    // this was so I could measure timing with the oscilloscope
     pinMode(D0, OUTPUT); // Set D0 as an output
+
+    //basic LED stuff
+    pinMode(LED_RED, OUTPUT);
+    pinMode(LED_GREEN, OUTPUT);
+    pinMode(LED_BLUE, OUTPUT);
+    
+    digitalWrite(LED_RED, HIGH);
+    digitalWrite(LED_GREEN, HIGH);
+    digitalWrite(LED_BLUE, work_LED_status);
+
+    QSPIF_sleep();
+
+    pinMode(DO_WORK_PIN, INPUT_PULLUP_SENSE);
+    attachInterrupt(digitalPinToInterrupt(DO_WORK_PIN), doWorkISR, FALLING);
+
+    pinMode(SHUTDOWN_PIN, INPUT_PULLUP_SENSE);
+    attachInterrupt(digitalPinToInterrupt(SHUTDOWN_PIN), shutdownISR, FALLING);
+
+    xSemaphore = xSemaphoreCreateBinary();
+
+    // Flash green to see power on, reset, and wake from system_off
+    digitalWrite(LED_GREEN, LOW);
+    delay(1000);
+    digitalWrite(LED_GREEN, HIGH);
+    boot_millis = millis();
+}
+
+void doWorkISR()
+{
+  xSemaphoreGive(xSemaphore);
+}
+
+void shutdownISR()
+{
+  gotoSystemOffSleep = true;
+  xSemaphoreGive(xSemaphore);
 }
 
 uint8_t sample_fast(){
-  analogWrite(LEDB, 0);//turn on blue LED
+  digitalWrite(LED_BLUE, LOW);//turn on blue LED
   digitalWrite(D0, HIGH);
   for(int i=0; i<fast_buf_length; i++){
     x_val = myIMU.readFloatAccelX();
@@ -114,7 +163,7 @@ uint8_t sample_fast(){
     fast_buffer.push(gs_int);
   }
   digitalWrite(D0, LOW);
-  analogWrite(LEDB, 255);//turn off blue LED
+  digitalWrite(LED_BLUE, LOW);//turn off blue LED
 
   uint8_t max_gs = 0;
   for(int i=0; i<fast_buf_length; i++){
@@ -129,6 +178,28 @@ uint8_t sample_fast(){
 
 
 void loop() {    
+    // code to put the system into deep sleep if it's been on for over 10 min
+    if(millis() - boot_millis > 20000){
+      gotoSystemOffSleep = true;
+    }
+
+    if (gotoSystemOffSleep)
+    {
+      //Turn off the blue LED
+      digitalWrite(LED_BLUE, HIGH);
+      //Flash red to see we are going to system_off sleep mode
+      digitalWrite(LED_RED, LOW);
+      delay(1000);
+      digitalWrite(LED_RED, HIGH);
+
+      NRF_POWER->SYSTEMOFF=1; // Execution should not go beyond this
+      //sd_power_system_off() // Use this instead if using the soft device
+    }
+    // Not going to system off sleep mode, so do work
+    work_LED_status = !work_LED_status;
+    digitalWrite(LED_BLUE, work_LED_status);
+    
+    
     gs_int = sample_fast();
     slow_buffer.push(gs_int);
 
@@ -157,7 +228,7 @@ void loop() {
     // Serial.println(gs);
     //########################Serial is for debugging only#############
 
-    // analogWrite(LEDG, 255);// turn off green LED
+    // digitalWrite(LED_GREEN, HIGH);// turn off green LED
     // delay(100);
-    // analogWrite(LEDG, 0);
+    // digitalWrite(LED_GREEN, LOW);
 }
