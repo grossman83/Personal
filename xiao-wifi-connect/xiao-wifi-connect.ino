3685ba58e5b6c19870d1a01d158dc26ac84ee794
MARC GROSSMAN <marc@marcbookair.local>
2024-04-26 09:19:40 -0700

    compiles correctly.

--- xiao-wifi-connect/xiao-wifi-connect.ino
+++ xiao-wifi-connect/xiao-wifi-connect.ino
@@ -1,28 +1,32 @@
 #include <WiFi.h>
-#include <ArduinoIoTCloud.h>
-#include <Arduino_ConnectionHandler.h>
 
-const char ssid[] = "BigLukeNetwork";           // Your WiFi SSID
-const char pass[] = "lukethedog";       // Your WiFi password
+const char* ssid     = "TheTubes2.4";
+const char* password = "TedStevens";   
 
-void setup() {
-  Serial.begin(115200);
-  // Attempt to connect to WiFi network:
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print("Attempting to connect to WPA SSID: ");
+void setup()
+{
+    Serial.begin(115200);
+    delay(10);
+
+    // We start by connecting to a WiFi network
+
+    Serial.println();
+    Serial.println();
+    Serial.print("Connecting to ");
     Serial.println(ssid);
-    // Connect to WPA/WPA2 network:
-    WiFi.begin(ssid, pass);
-    // Wait 10 seconds for connection:
-    delay(10000);
+
+    WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+}  
+void loop()
+{
   }
-
-  // Print WiFi status:
-  Serial.println("You're connected to the network");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
-}
-
-void loop() {
-  // Add your repeated code here
-}
