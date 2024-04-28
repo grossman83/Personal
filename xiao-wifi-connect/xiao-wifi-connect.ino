c7040b80582aeffaf92d65d210a0c664a38f6ae6
MARC GROSSMAN <marc@marcbookair.local>
2024-04-28 13:07:32 -0700

    Connects to WiFi Outputs a reasonable RMS on A1 of 530-550 mV when measuring ~10 amps. Does this all at a very rapid rate. Still to be determined how many samples I should actually take to get fair averaging.

--- xiao-wifi-connect/xiao-wifi-connect.ino
+++ xiao-wifi-connect/xiao-wifi-connect.ino
@@ -1,7 +1,21 @@
 #include <WiFi.h>
+// #include "CircularBuffer.hpp"
 
-const char* ssid     = "TheTubes2.4";
-const char* password = "TedStevens";   
+const char* ssid     = "BigLukeNetwork";
+const char* password = "lukethedog";
+
+const int array_length = 1000;
+const float a0_middle = 1062;
+const float int2mV = 0.805664;
+const float middlemV = 3.3/2.0;
+
+float Analog0 = 0;
+float Analog1 = 0;
+float Analog2 = 0;
+float Analog3 = 0;
+
+// CircularBuffer<uint8_t, fast_buf_length> fast_buffer;
+// CircularBuffer<double, 1000> A0_buffer;
 
 void setup()
 {
@@ -26,7 +40,58 @@ void setup()
     Serial.println("WiFi connected");
     Serial.println("IP address: ");
     Serial.println(WiFi.localIP());
+
+    pinMode(A0, INPUT); //ADC
+    pinMode(A1, INPUT);
+    pinMode(A2, INPUT);
+    pinMode(A3, INPUT);
+    pinMode(D7, OUTPUT);
 }  
+
+
+double readAnalog(){
+  float a1_array[array_length];
+
+  digitalWrite(D7, HIGH);
+  for(int k=0; k<array_length; k++){
+    a1_array[k] = analogReadMilliVolts(A1)/1000.0;
+  }
+  digitalWrite(D7, LOW);
+
+  //now for the RMS part
+  double a0_rms = 0;
+  double a1_rms = 0;
+  double a2_rms = 0;
+  double a3_rms = 0;
+  for(int k=0; k<array_length; k++){
+    a1_rms = a1_rms + pow(a1_array[k]-middlemV, 2);
+  }
+  a1_rms = a1_rms/array_length;
+
+  a1_rms = sqrt(a1_rms);
+
+  return a1_rms;
+}
+
+
+
 void loop()
 {
-  }
+  digitalWrite(D7, HIGH);
+  Analog0 = analogRead(A0);
+  Analog1 = readAnalog();
+  Analog2 = analogRead(A2);
+  Analog3 = analogRead(A3);
+
+
+
+  // Serial.println(Analog0);
+  Serial.println(Analog1);
+  // Serial.println(Analog2);
+  // Serial.println(Analog3);
+
+
+  delay(10);
+  digitalWrite(D7, LOW);
+  delay(10);
+}
