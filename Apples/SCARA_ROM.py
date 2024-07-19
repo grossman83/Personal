c8601376aaae3e7c60616772cc5be4d21c850db8
Marc Grossman <marc.a.grossman@gmail.com>
2024-07-19 14:00:12 -0700

    I've added the ability to plot only points that fall within the torques and speeds of the motor/gearbox.

--- Apples/SCARA_ROM.py
+++ Apples/SCARA_ROM.py
@@ -16,12 +16,50 @@ def make_axes_equal(fig):
 
 
 
+
+################################ INPUTS ######################################
 a1_length = 600
 a2_length = 600
 
+a1_mass = 3.6#kg
+a2_mass = 3.85#kg
 
+a1_arm = 0.213#m
+a2_arm = 0.257#m
+
+q1_max_torque = 8#N*m
+q2_max_torque = 8#N*m
+
+
+a1_reduction = 10
+a2_reduction = 10
+
+max_omega_rpm = 1200
+max_torque = 100#N*m (omega**2 * inertia)
+
+max_cart_vel = 3000#mm/s
+accel_multiple = 10
 
 num_pts = 151
+################################ INPUTS ######################################
+
+
+
+
+
+
+################################ DERIVED ######################################
+a1_inertia = a1_mass * a1_arm**2
+a2_inertia = a2_mass * a2_arm**2
+max_omega1 = max_omega_rpm * 2 * np.pi / 60 / a1_reduction
+max_omega2 = max_omega_rpm * 2 * np.pi / 60 / a2_reduction
+max_cart_accel = max_cart_vel * accel_multiple
+
+gearbox1_max_torque = q1_max_torque * a1_reduction
+gearbox2_max_torque = q2_max_torque * a2_reduction
+################################ DERIVED ######################################
+
+
 
 xpos = np.linspace(0,1500, num_pts)
 zpos = np.linspace(-1500, 1500, num_pts)
@@ -111,41 +149,84 @@ new_zmesh = new_mesh[1]
 new_q2_up = np.arccos((np.square(new_xmesh) + np.square(new_zmesh) - np.square(a1) - np.square(a2))/(2*a1*a2))
 new_q1_up = np.arctan(new_zmesh/new_xmesh) - np.arctan((a2*np.sin(q2_up))/(a1+a2*np.cos(q2_up)))
 
-
-# cart_vel = 3000#mm/s
-
-def get_ratio(dthetadcart):
-	ratio = np.nanmax(np.abs(dthetadcart)) / np.nanmin(np.abs(dthetadcart))
-	return the_ratio
+#dtheta/dt = dtheta/dx * dx/dt (where dx/dt is max_cart_vel) [rad/s]
+#dtheta/dt^2 = dtheta/dx * dx/dt * dx/dt [rad/s2]
 
 
 
 
+# not being used anyways.
+# def get_ratio(dthetadcart):
+# 	ratio = np.nanmax(np.abs(dthetadcart)) / np.nanmin(np.abs(dthetadcart))
+# 	return the_ratio
 
 
 dtheta1dx = (new_q1_up - q1_up)/delta#*cart_vel
 dtheta2dx = (new_q2_up - q2_up)/delta#*cart_vel
 
+dtheta1dt = dtheta1dx * max_cart_vel
+dtheta2dt = dtheta2dx * max_cart_vel
+
+dtheta1dtdt = dtheta1dt * max_cart_accel
+dtheta2dtdt = dtheta2dt * max_cart_accel
+
+
+
 # condition for bit-masking the stuff that is out of the ROM.
 # only need to check dtheta1 because it is based on dtheta 2 anyhow
-condition  = np.isnan(dtheta1dx)
+nancondition  = np.isnan(dtheta1dx)
+# now various maskings to make sure that we're below the max motor speed
+speed_condition = np.logical_and(
+	np.abs(dtheta1dt)<max_omega1,
+	np.abs(dtheta2dt)<max_omega2
+	)
 
+#calculate torques.
+t2 = dtheta2dtdt * a2_inertia
+tg2 = -1 * a2_mass * a2_arm * np.cos(q2_up+q1_up)#gravity results in - torque
+t2 = t2 + tg2
+
+t1 = dtheta1dtdt*a1_inertia - np.cos(q1_up)*a1_mass*a1_arm + t2
+
+torque_condition = np.logical_and(
+	np.abs(t1<gearbox1_max_torque), np.abs(t2<gearbox2_max_torque))
+
+condition = np.logical_and(speed_condition, torque_condition)
+
+
+#combine all conditions
+condition = np.logical_and(~nancondition, condition)
+
+
+#Trying a way to combine many logical arrays into one with and
+# condition_test = np.logical_and.reduce((
+# 	nancondition,
+# 	np.abs(dtheta1dt)<max_omega1,
+# 	np.abs(dtheta2dt)<max_omega2
+# 	 ))
+
+
+
+# pdb.set_trace()
 
 
 dtheta1dx_plot = go.Scatter(
-    x=xmesh[~condition].flatten(),
-    y=zmesh[~condition].flatten(),
+    x=xmesh[condition].flatten(),
+    y=zmesh[condition].flatten(),
     name='dtheta1/dx',
     mode='markers',
     marker=dict(
         size=3,
-        color=np.abs(dtheta1dx[~condition].flatten()),
+        color=np.abs(dtheta1dx[condition].flatten()),
         colorscale='plasma',
         colorbar=dict(title="dtheta1/dx"),
         coloraxis="coloraxis1"  # Link to the first color axis
     ),
     hovertemplate='%{marker.color:.4f}<extra></extra>'
 )
+fig.add_trace(dtheta1dx_plot, row=2, col=1,)
+fig.show()
+pdb.set_trace()
 
 dtheta2dx_plot = go.Scatter(
     x=xmesh[~condition].flatten(),
@@ -250,18 +331,35 @@ fig.show()
 pdb.set_trace()
 
 
-def getLinearPoints(xs, zs, duration, max_speed, max_accel, num_pts):
-	dx = xs[1] - xs[0]
-	dz = zs[1] - zs[0]
+# this was meant to work on the motion and timing. I'll come back to it later.
+# def getLinearPoints(x_if, z_if, duration, max_speed, max_accel, num_pts):
+# 	dx = x_if[1] - x_if[0]
+# 	dz = z_if[1] - z_if[0]
+# 	dt = 
 
-	path_length = np.sqrt(dx**2 + dy**2)
-	max_accel_duration = max_speed/max_accel
-	if duration > 2*max_accel_duration:
-		#the path is long enough that we're going to get a trapezoidal path
-		pass
+# 	path_length = np.sqrt(dx**2 + dy**2)
+# 	max_accel_duration = max_speed/max_accel
+# 	if duration > 2*max_accel_duration:
+# 		#the path is long enough that we're going to get a trapezoidal path
+# 		xs = np.zeros(num_pts)
+# 		zs = np.zeros(num_pts)
+
+# 		xs[0] = x_if[0]
+# 		xs[-1] = x_if[1]
+
+# 		#determine if max accel is needed to reach max speed not overshoot
+# 		if (dx < 0.5*max_accel*dt**2):
+# 			#accel at max velosity for timestep
 
 
-	return 0
+
+
+
+# 	if duration <= 2*max_accel_duration:
+# 		pass
+
+
+# 	return 0
 
 
 
